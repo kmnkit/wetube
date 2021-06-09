55cc7e85a25506eb18bf160ca9fa7f9818503535
Marco Kang <kmnkit@gmail.com>
2021-06-09 17:06:03 +0900

    thumbnail

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,12 +6,36 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+    const ffmpeg = createFFmpeg({ log: true });
+    await ffmpeg.load();
+
+    ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+    await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+    await ffmpeg.run("-i", "recording.webm", "-ss", "00:00:01", "-frames:v", "1", "thumbnail.jpg");
+
+    const mp4File = ffmpeg.FS("readFile", "output.mp4");
+    const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+
+    const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+    const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
+
+    const mp4Url = URL.createObjectURL(mp4Blob);
+    const thumbUrl = URL.createObjectURL(thumbBlob);
+
     const a = document.createElement("a");
-    a.href = videoFile;
-    a.download = "MyRecording.webm";
+    a.href = mp4Url;
+    a.download = "MyRecording.mp4";
     document.body.appendChild(a);
     a.click();
+
+    const thumbA = document.createElement("a");
+    thumbA.href = thumbUrl;
+    thumbA.download = "thumbnail.jpg";
+    document.body.appendChild(thumbA);
+    thumbA.click();
 }
 
 const handleStop = () => {
@@ -36,7 +61,7 @@ const handleStart = () => {
 }
 
 const init = async () => {
-    stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
     video.srcObject = stream;
     video.play();
 }

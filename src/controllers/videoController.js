3794dc4431459fd680610bf3457b61f3161be419
Marco Kang <kmnkit@gmail.com>
2021-06-15 15:38:40 +0900

    video controller edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -89,7 +89,7 @@ export const postUpload = async (req, res) => {
             title,
             description,
             fileUrl: res.locals.isHeroku ? video[0].location : video[0].path,
-            thumbUrl: res.locals.isHeroku ? thumb[0].location : video[0].path,
+            thumbUrl: res.locals.isHeroku ? thumb[0].location : thumb[0].path,
             owner,
             hashtags: Video.formatHashtags(hashtags),
         });
